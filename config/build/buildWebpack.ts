cfa905161617c578781bf6ea5c3d75eda36cff79
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-10-22 22:34:11 +0300

    + webpack decomposition

---
+++ config/build/buildWebpack.ts
@@ -0,0 +1,33 @@
+import path from 'path';
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { buildDevServer } from './buildDevServer';
+import { buildLoaders } from './buildLoaders';
+import { buildPlugins } from './buildPlugins';
+import { buildResolvers } from './buildResolvers';
+import { BuildOptions } from './types/types';
+
+export function buildWebpack(options: BuildOptions): webpack.Configuration {
+    const {mode, paths} = options
+    const isDev = mode === 'development'
+
+    return {
+        mode: mode ?? 'development',
+        entry: paths.entry,
+        output: {
+          path: paths.output,
+          filename: '[name].[contenthash].js',
+          clean: true,
+        },
+        plugins: buildPlugins(options),
+        module: {
+          rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(options),
+        devtool: isDev ? 'inline-source-map' : false,
+        devServer: isDev ? buildDevServer(options) : undefined,
+      }
+
+}
