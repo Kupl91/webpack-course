434410df5a0b41f6f9d0db031fdae4118c4860e8
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-10-23 17:23:45 +0300

    webpack-bundle-analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,27 +2,34 @@ import { Configuration } from "webpack";
 import { BuildOptions } from "./types/types";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import webpack from 'webpack'
+import webpack from 'webpack';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
+export function buildPlugins({ mode, paths, analyzer }: BuildOptions): Configuration['plugins'] {
+  const isDev = mode === 'development';
+  const isProd = mode === 'production';
 
-export function buildPlugins({mode, paths}: BuildOptions): Configuration['plugins'] {
-    const isDev = mode === 'development';
-    const isProd = mode === 'production';
+  const plugins: Configuration['plugins'] = [
+    new HtmlWebpackPlugin({ template: paths.html })
+  ];
+
+  if (isDev) {
+    plugins.push(new webpack.ProgressPlugin());
+  }
+
+  if (isProd) {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css',
+      })
+    );
     
-    const plugins: Configuration['plugins'] = [
-        new HtmlWebpackPlugin({ template: paths.html })
-    ];
+  }
 
-    if (isDev) {
-        plugins.push(new webpack.ProgressPlugin())
-    }
+  if(analyzer) {
+    plugins.push(new BundleAnalyzerPlugin());
+  }
 
-    if (isProd) {
-        plugins.push(new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
-        }));
-    }
-
-    return plugins; 
+  return plugins;
 }
