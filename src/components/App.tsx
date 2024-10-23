9612bf77d55b2c46c29476e1f8add9987a272d99
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-10-23 14:21:00 +0300

    import { Outlet, Link } from 'react-router-dom'

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import classes from './App.module.scss'
+import { Outlet, Link } from 'react-router-dom';
+
 
 const App = () => {
     const [count, setCount] = useState<number>(0)
@@ -7,8 +9,12 @@ const App = () => {
     const increment = () => setCount(prev => prev + 1)
   return (
     <div>
+      <Link to ={'/about'}>about</Link>
+      <br/>
+      <Link to ={'/shop'}>shop</Link>
         <h1 className={classes.value}>{count}</h1>
         <button className={classes.button} onClick={increment}>+1</button>
+        <Outlet/>
     </div>
   )
 }
